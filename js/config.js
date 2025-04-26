cb170f41c3a05cf2af8a09d4357a4411af91ff3f
shinya <kobayashi_shinya@outlook.com>
2025-04-27 02:49:06 +0800

    refactor(广告过滤): 优化M3U8广告过滤逻辑

--- js/config.js
+++ js/config.js
@@ -24,12 +24,14 @@ const API_SITES = {
     heimuer: {
         api: 'https://json.heimuer.xyz',
         name: '黑木耳',
-        detail: 'https://heimuer.tv'
+        detail: 'https://heimuer.tv',
+        filterAdRule: `#EXT-X-DISCONTINUITY\\n#EXTINF:\\d+\\.\\d+,\\n.*?\\n#EXT-X-DISCONTINUITY`
     },
     ffzy: {
         api: 'http://ffzy5.tv',
         name: '非凡影视',
-        detail: 'http://ffzy5.tv'
+        detail: 'http://ffzy5.tv',
+        filterAdRule: '#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:6.666667,[\\s\\S]*?#EXT-X-DISCONTINUITY'
     },
     tyyszy: {
         api: 'https://tyyszy.com',
@@ -67,12 +69,10 @@ const API_SITES = {
     mdzy: {
         api: 'https://www.mdzyapi.com',
         name: '魔都资源',
-        filterAd: false,
     },
     ruyi: {
         api: 'https://cj.rycjapi.com',
         name: '如意资源',
-        filterAd: false,
     },
     zuid: {
         api: 'https://api.zuidapi.com',
